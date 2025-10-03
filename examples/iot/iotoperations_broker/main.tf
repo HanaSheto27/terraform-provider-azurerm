5b33ebad05213d68d62578150548ae1a7f691b38
Hana Sheto <hanasheto@microsoft.com>
2025-10-03 13:04:38 -0700

    updating the broker to workwith azarm

--- examples/iot/iotoperations_broker/main.tf
+++ examples/iot/iotoperations_broker/main.tf
@@ -1,9 +1,7 @@
 terraform {
+  required_version = ">= 1.6"
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
   }
 }
 
@@ -11,53 +9,77 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+# ---------- Vars ----------
+variable "resource_group_name" { type = string }          # existing RG
+variable "instance_name"       { type = string }          # existing AIO instance name
+
+variable "broker_name" {
+  type        = string
+  default     = "example-broker"
+  description = "Name for the broker to create under existing instance"
 }
 
-# IoT Operations Instance (prerequisite for broker)
-resource "azurerm_iotoperations_instance" "example" {
-  name                = "example-iot-instance"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+variable "custom_location_id"  { type = string }          # ARM ID of Custom Location
 
-  # Extended location for IoT Operations
-  extended_location {
-    name = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.example.name}/providers/Microsoft.ExtendedLocation/customLocations/example-location"
-    type = "CustomLocation"
-  }
+variable "memory_profile" {
+  type        = string
+  default     = "Medium"
+  description = "Broker memory profile (string enum)."
 }
 
-# IoT Operations Broker
-resource "azurerm_iotoperations_broker" "example" {
-  name                = "example-broker"
-  resource_group_name = azurerm_resource_group.example.name
-  instance_name       = azurerm_iotoperations_instance.example.name
-
-  properties {
-    memory_profile = "Medium"
-  }
-
-  extended_location {
-    name = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.example.name}/providers/Microsoft.ExtendedLocation/customLocations/example-location"
-    type = "CustomLocation"
-  }
+# ---------- Data ----------
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
 }
 
-data "azurerm_client_config" "current" {}
+# ---------- Deploy Broker (child of existing Instance) ----------
+resource "azurerm_resource_group_template_deployment" "iotops_broker" {
+  name                = "${var.broker_name}-deploy"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  deployment_mode     = "Incremental"
+
+  template_content = <<JSON
+{
+  "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "instanceName":     { "type": "string" },
+    "brokerName":       { "type": "string" },
+    "customLocationId": { "type": "string" },
+    "memoryProfile":    { "type": "string" }
+  },
+  "resources": [
+    {
+      "type": "Microsoft.IoTOperations/instances/brokers",
+      "apiVersion": "2024-11-01",
+      "name": "[format('{0}/{1}', parameters('instanceName'), parameters('brokerName'))]",
+      "extendedLocation": {
+        "type": "CustomLocation",
+        "name": "[parameters('customLocationId')]"
+      },
+      "properties": {
+        "memoryProfile": "[parameters('memoryProfile')]"
+      }
+    }
+  ],
+  "outputs": {
+    "brokerId": {
+      "type": "string",
+      "value": "[resourceId('Microsoft.IoTOperations/instances/brokers', parameters('instanceName'), parameters('brokerName'))]"
+    }
+  }
+}
+JSON
+
+  parameters_content = jsonencode({
+    instanceName     = { value = var.instance_name }
+    brokerName       = { value = var.broker_name }
+    customLocationId = { value = var.custom_location_id }
+    memoryProfile    = { value = var.memory_profile }
+  })
+}
 
 output "broker_id" {
-  description = "The ID of the IoT Operations Broker"
-  value       = azurerm_iotoperations_broker.example.id
-}
-
-output "broker_name" {
-  description = "The name of the IoT Operations Broker"
-  value       = azurerm_iotoperations_broker.example.name
-}
-
-output "instance_name" {
-  description = "The name of the IoT Operations Instance"
-  value       = azurerm_iotoperations_instance.example.name
+  value       = jsondecode(azurerm_resource_group_template_deployment.iotops_broker.output_content).brokerId.value
+  description = "ARM ID of the Broker."
 }
