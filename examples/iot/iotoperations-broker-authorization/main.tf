3f2feeffe5b5c1470df185dce1bd605cd1e755ff
Hana Sheto <hanasheto@microsoft.com>
2025-10-04 14:28:53 -0700

    brokerauthorization terraform file update

--- examples/iot/iotoperations-broker-authorization/main.tf
+++ examples/iot/iotoperations-broker-authorization/main.tf
@@ -5,21 +5,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 2.0"
-    }
   }
 }
 
-# ---------- Providers ----------
 provider "azurerm" {
   features {}
 }
 
-provider "azapi" {}
-
-# variables needed
+# ---------------- Vars ----------------
 variable "resource_group_name" {
   type        = string
   default     = "example-iotoperations"
@@ -47,24 +40,21 @@ variable "broker_name" {
 variable "authorization_name" {
   type        = string
   default     = "terraformauthorization"
-  description = "Name of the IoT Operations broker authorization resource."
+  description = "Name of the IoT Operations broker authentication resource."
 }
 
-# FULL ARM ID of your Custom Location (Arc-enabled K8s)
 # /subscriptions/<subId>/resourceGroups/<rg>/providers/Microsoft.ExtendedLocation/customLocations/<customLocationName>
 variable "custom_location_id" {
   type        = string
   description = "ARM ID of the Custom Location used by AIO."
 }
 
-# FULL ARM ID of your Device Registry Schema Registry:
 # /subscriptions/<subId>/resourceGroups/<rg>/providers/Microsoft.DeviceRegistry/schemaRegistries/<schemaRegistryName>
 variable "schema_registry_id" {
   type        = string
   description = "ARM ID of the Schema Registry referenced by the AIO instance."
 }
 
-# Optional: tags
 variable "tags" {
   type = map(string)
   default = {
@@ -73,162 +63,210 @@ variable "tags" {
   }
 }
 
-# -Data (useful for subscription)
 data "azurerm_client_config" "current" {}
 
-# Resources
+# ---------------- RG ----------------
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
+  tags     = var.tags
 }
 
-# IoT Operations Instance via AzAPI (ARM: Microsoft.IoTOperations/instances@2024-11-01)
-resource "azapi_resource" "iotops_instance" {
-  type      = "Microsoft.IoTOperations/instances@2024-11-01"
-  name      = var.instance_name
-  parent_id = azurerm_resource_group.example.id
-  location  = azurerm_resource_group.example.location
-  tags      = var.tags
+# ---------------- AIO Instance (ARM: Microsoft.IoTOperations/instances@2024-11-01) ----------------
+resource "azurerm_resource_group_template_deployment" "iotops_instance" {
+  name                = "${var.instance_name}-deploy"
+  resource_group_name = azurerm_resource_group.example.name
+  deployment_mode     = "Incremental"
 
-  # NOTE: For AzAPI v2, body must be object
-  body = {
-    # REQUIRED: AIO deploys into a Custom Location (Arc-enabled K8s)
-    extendedLocation = {
-      type = "CustomLocation"
-      name = var.custom_location_id
-    }
-
-    properties = {
-      # 'version' is READ-ONLY in this API version; do NOT include it.
-      description = "Example IoT Operations instance managed by Terraform"
-
-      # REQUIRED: reference an existing Schema Registry by ARM ID
-      schemaRegistryRef = {
-        resourceId = var.schema_registry_id
+  template_content = <<JSON
+{
+  "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "instanceName":     { "type": "string" },
+    "location":         { "type": "string" },
+    "customLocationId": { "type": "string" },
+    "schemaRegistryId": { "type": "string" },
+    "tags":             { "type": "object" }
+  },
+  "resources": [
+    {
+      "type": "Microsoft.IoTOperations/instances",
+      "apiVersion": "2024-11-01",
+      "name": "[parameters('instanceName')]",
+      "location": "[parameters('location')]",
+      "extendedLocation": { "type": "CustomLocation", "name": "[parameters('customLocationId')]" },
+      "tags": "[parameters('tags')]",
+      "properties": {
+        "description": "IoT Operations instance (Terraform via AzureRM template)",
+        "schemaRegistryRef": { "resourceId": "[parameters('schemaRegistryId')]" }
       }
     }
+  ],
+  "outputs": {
+    "instanceId": { "type": "string", "value": "[resourceId('Microsoft.IoTOperations/instances', parameters('instanceName'))]" }
   }
 }
+JSON
 
-# IoT Operations Broker via AzAPI (ARM: Microsoft.IoTOperations/instances/brokers@2024-11-01)
-resource "azapi_resource" "iotops_broker" {
-  type      = "Microsoft.IoTOperations/instances/brokers@2024-11-01"
-  name      = var.broker_name
-  parent_id = azapi_resource.iotops_instance.id
-
-  body = {
-    extendedLocation = {
-      type = "CustomLocation"
-      name = var.custom_location_id
-    }
-
-    properties = {
-      memoryProfile = "Medium"
-    }
-  }
-
-  depends_on = [azapi_resource.iotops_instance]
+  parameters_content = jsonencode({
+    instanceName     = { value = var.instance_name }
+    location         = { value = var.location }
+    customLocationId = { value = var.custom_location_id }
+    schemaRegistryId = { value = var.schema_registry_id }
+    tags             = { value = var.tags }
+  })
 }
 
-# IoT Operations Broker Authorization
-resource "azurerm_broker_authorization" "example" {
-  name                = var.authorization_name
+# ---------------- Broker (ARM: Microsoft.IoTOperations/instances/brokers@2024-11-01) ----------------
+resource "azurerm_resource_group_template_deployment" "iotops_broker" {
+  name                = "${var.broker_name}-deploy"
   resource_group_name = azurerm_resource_group.example.name
-  instance_name       = azapi_resource.iotops_instance.name
-  broker_name         = azapi_resource.iotops_broker.name
+  deployment_mode     = "Incremental"
 
-  authorization_policies {
-    type = "RoleBasedAccessControl"
-    
-    rbac_config {
-      principals = [
-        {
-          usernames = ["user1", "user2"]
-          attributes = {
-            "building" = "17"
-            "floor"    = "1"
-          }
-        }
-      ]
-
-      resources = [
-        {
-          method = "Connect"
-          topics = ["temperature/*", "humidity/*"]
-        },
-        {
-          method = "Publish"
-          topics = ["alerts/*"]
-        },
-        {
-          method = "Subscribe"
-          topics = ["commands/*"]
-        }
+  template_content = <<JSON
+{
+  "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "instanceName":     { "type": "string" },
+    "brokerName":       { "type": "string" },
+    "customLocationId": { "type": "string" },
+    "tags":             { "type": "object" }
+  },
+  "resources": [
+    {
+      "type": "Microsoft.IoTOperations/instances/brokers",
+      "apiVersion": "2024-11-01",
+      "name": "[format('{0}/{1}', parameters('instanceName'), parameters('brokerName'))]",
+      "extendedLocation": { "type": "CustomLocation", "name": "[parameters('customLocationId')]" },
+      "tags": "[parameters('tags')]",
+      "properties": { "memoryProfile": "Medium" },
+      "dependsOn": [
+        "[resourceId('Microsoft.IoTOperations/instances', parameters('instanceName'))]"
       ]
     }
+  ],
+  "outputs": {
+    "brokerId": {
+      "type": "string",
+      "value": "[resourceId('Microsoft.IoTOperations/instances/brokers', parameters('instanceName'), parameters('brokerName'))]"
+    }
   }
+}
+JSON
 
-  authorization_policies {
-    type = "AttributeBasedAccessControl"
-    
-    abac_config {
-      rules = [
-        {
-          principals = {
-            attributes = {
-              "department" = "engineering"
-              "clearance"  = "high"
+  parameters_content = jsonencode({
+    instanceName     = { value = var.instance_name }
+    brokerName       = { value = var.broker_name }
+    customLocationId = { value = var.custom_location_id }
+    tags             = { value = var.tags }
+  })
+
+  depends_on = [azurerm_resource_group_template_deployment.iotops_instance]
+}
+
+# ---------------- Broker Authentication (ARM: .../brokers/authentications@2024-11-01) ----------------
+resource "azurerm_resource_group_template_deployment" "iotops_broker_auth" {
+  name                = "${var.authorization_name}-deploy"
+  resource_group_name = azurerm_resource_group.example.name
+  deployment_mode     = "Incremental"
+
+  template_content = <<JSON
+{
+  "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "instanceName":       { "type": "string" },
+    "brokerName":         { "type": "string" },
+    "authenticationName": { "type": "string" },
+    "customLocationId":   { "type": "string" }
+  },
+  "resources": [
+    {
+      "type": "Microsoft.IoTOperations/instances/brokers/authentications",
+      "apiVersion": "2024-11-01",
+      "name": "[format('{0}/{1}/{2}', parameters('instanceName'), parameters('brokerName'), parameters('authenticationName'))]",
+      "extendedLocation": { "type": "CustomLocation", "name": "[parameters('customLocationId')]" },
+      "properties": {
+        "authenticationMethods": [
+          {
+            "method": "ServiceAccountToken",
+            "serviceAccountTokenSettings": { "audiences": ["audience1", "audience2"] }
+          },
+          {
+            "method": "X509",
+            "x509Settings": {
+              "trustedClientCaCert": "example-ca-cert",
+              "authorizationAttributes": { "building": "17", "floor": "1" }
+            }
+          },
+          {
+            "method": "Custom",
+            "customSettings": {
+              "auth": { "x509": { "secretRef": "example-secret" } },
+              "caCertConfigMap": "example-ca-configmap",
+              "endpoint": "https://example-auth-endpoint.com",
+              "headers": {
+                "X-Custom-Header": "example-value",
+                "Authorization":   "Bearer token"
+              }
             }
           }
-          
-          resources = {
-            topics = ["sensitive/*"]
-            clientIds = ["trusted-client-*"]
-          }
-          
-          operations = ["Connect", "Publish", "Subscribe"]
-        }
+        ]
+      },
+      "dependsOn": [
+        "[resourceId('Microsoft.IoTOperations/instances/brokers', parameters('instanceName'), parameters('brokerName'))]"
       ]
     }
+  ],
+  "outputs": {
+    "authenticationId": {
+      "type": "string",
+      "value": "[resourceId('Microsoft.IoTOperations/instances/brokers/authentications', parameters('instanceName'), parameters('brokerName'), parameters('authenticationName'))]"
+    }
   }
+}
+JSON
 
-  # Extended location for Arc-enabled Kubernetes
-  extended_location {
-    name = var.custom_location_id
-    type = "CustomLocation"
-  }
+  parameters_content = jsonencode({
+    instanceName       = { value = var.instance_name }
+    brokerName         = { value = var.broker_name }
+    authenticationName = { value = var.authorization_name }
+    customLocationId   = { value = var.custom_location_id }
+  })
 
-  depends_on = [azapi_resource.iotops_broker]
+  depends_on = [azurerm_resource_group_template_deployment.iotops_broker]
 }
 
-# ---------- Outputs ----------
+# ---------------- Outputs ----------------
 output "iotoperations_instance_id" {
-  value       = azapi_resource.iotops_instance.id
-  description = "ARM ID of the created IoT Operations instance."
+  description = "ARM ID of the AIO instance."
+  value       = jsondecode(azurerm_resource_group_template_deployment.iotops_instance.output_content).instanceId.value
 }
 
 output "iotoperations_broker_id" {
-  value       = azapi_resource.iotops_broker.id
-  description = "ARM ID of the created IoT Operations broker."
+  description = "ARM ID of the AIO broker."
+  value       = jsondecode(azurerm_resource_group_template_deployment.iotops_broker.output_content).brokerId.value
 }
 
 output "broker_authorization_id" {
-  value       = azurerm_broker_authorization.example.id
-  description = "ARM ID of the created IoT Operations broker authorization."
+  description = "ARM ID of the AIO broker authentication."
+  value       = jsondecode(azurerm_resource_group_template_deployment.iotops_broker_auth.output_content).authenticationId.value
 }
 
 output "broker_authorization_name" {
-  value       = azurerm_broker_authorization.example.name
-  description = "Name of the IoT Operations broker authorization."
+  description = "Name of the AIO broker authentication."
+  value       = var.authorization_name
 }
 
 output "resource_hierarchy" {
+  description = "Complete resource hierarchy for the IoT Operations broker authentication."
   value = {
     subscription_id    = data.azurerm_client_config.current.subscription_id
     resource_group     = azurerm_resource_group.example.name
-    instance_name      = azapi_resource.iotops_instance.name
-    broker_name        = azapi_resource.iotops_broker.name
-    authorization_name = azurerm_broker_authorization.example.name
+    instance_name      = var.instance_name
+    broker_name        = var.broker_name
+    authorization_name = var.authorization_name
   }
-  description = "Complete resource hierarchy for the IoT Operations broker authorization."
 }
